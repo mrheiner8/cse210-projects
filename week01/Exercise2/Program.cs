205ec1efcc496011556c8264ad6b6ae5614c033f
mrheiner8 <mrheiner8@gmail.com>
2026-09-02 22:36:27 -0600

    week 1 exercise 2 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,79 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("\nHello World! This is the Exercise2 Project.");
+        
+        // Ask the user for a grade in percentage.
+        Console.Write("\nWhat is grade percentage? ");
+        string gradePercent = Console.ReadLine();
+
+        // Setup variables for latter
+        int grade = int.Parse(gradePercent);
+        string letter;
+        int lastDigit = (grade % 10);
+        string sign;
+
+        // Determines the letter grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign for the grade
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // Handle A+ grades 
+        // Grade >= 97 catches only last digits 7-9, since 93-96 already resolve to no sign
+        if (grade >= 97)
+        {
+            sign = "";
+        }
+
+        // Handle F- and F+ Grades
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+
+        // Tells the user if the class was passed
+        Console.WriteLine($"\nYour letter grade is: {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats, you passed!\n");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, you didn't pass.\n");
+        }
+
     }
 }
