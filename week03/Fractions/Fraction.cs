d7f97e5ce1d736fb136b8416bb84e2c22e679bc5
mrheiner8 <mrheiner8@gmail.com>
2026-09-15 22:22:08 -0600

    fraction skeleton

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,34 @@
+// Fraction.cs
+using System;
+
+
+// Create a class (custom data types) to use in Program
+public class Fraction
+{
+    // Member variables
+    private int _top;
+    private int _bottom;
+
+    // Constructors
+    /*
+    Fraction()
+    Fraction(wholeNumber : int)
+    Fraction(top : int, bottom : int)
+    */
+
+    // Getters and Setters
+    /*
+    GetTop()
+    SetTop(top : int)
+    GetBottom()
+    SetBottom(bottom : int)
+    */
+
+    // Methods
+    /*
+    GetFractionString() : string
+    GetDecimalValue() : double
+    */
+}
+
+// End Fraction.cs
